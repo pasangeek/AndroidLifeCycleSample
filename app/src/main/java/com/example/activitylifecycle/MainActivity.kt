2ff80fb05c8c2b8231f077c2c090a1f229c1f871
pasan wedikkara <pasan.madusanka9@gmail.com>
2023-01-12 22:46:42 +0900

    error corrected

--- app/src/main/java/com/example/activitylifecycle/MainActivity.kt
+++ app/src/main/java/com/example/activitylifecycle/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.activitylifecycle
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     private val TAG = MainActivity::class.java.simpleName
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         val second = findViewById<Button>(R.id.second)
         second.setOnClickListener {
 
-       val intent = Intent(this,ScondActivity::class.java)
+       val intent = Intent(this,SecondActivity::class.java)
       startActivity(intent)
             intent.putExtra("Uni_name","SLIIT")
             intent.putExtra("Extra_PHONE_number","010004551")
